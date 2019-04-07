fc7e1032af52a64b687d07e91ffd73ee6f136da4
novak <kelvin.onkundi@andela.com>
2019-04-07 13:01:20 +0300

    165167323-bug(src/components): Add a folder for each component 	- create a folder for each component 	- Everything related to the component is placed inside this folder i.e tests and styles.
    [Delivers #165167323]

--- src/routers/Approuter.js
+++ src/routers/Approuter.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
-import Login from '../components/Login'
-import Home from '../components/Home'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { Component } from 'react';
+import Login from '../components/Login/Login';
+import Home from '../components/Home/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Approuter = () => (
   <BrowserRouter>
     <div>
       <Switch>
-        <Route path='/' component={Home} exact />
-        <Route path='/login' component={Login} />
+        <Route path="/" component={Home} exact />
+        <Route path="/login" component={Login} />
       </Switch>
     </div>
   </BrowserRouter>
-)
+);
 
-export default Approuter
+export default Approuter;
